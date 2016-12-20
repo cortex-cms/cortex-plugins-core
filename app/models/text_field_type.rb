a3e59b21dce1f8305f01fae18cd359ab3767acbc
Peter Halloran <peter.halloran@careerbuilder.com>
2016-12-20 13:31:48 -0600

    added jsonb accessor dependency and uniqueness validations
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations
    
    added jsonb accessor dependency and uniqueness validations

--- app/models/text_field_type.rb
+++ app/models/text_field_type.rb
@@ -1,8 +1,10 @@
 class TextFieldType < FieldType
   attr_accessor :text
+  jsonb_accessor :data, text: :string
 
   validates :text, presence: true, if: :validate_presence?
   validate :text_length, if: :validate_length?
+  validate :text_unique, if: :validate_uniqueness?
 
   def data=(data_hash)
     @text = data_hash.deep_symbolize_keys[:text]
@@ -25,12 +27,22 @@ class TextFieldType < FieldType
   end
 
   def text_present
-    errors.add(:text, "must be present") if @text.empty?
+    errors.add(:text, 'must be present') if @text.empty?
   end
 
   def text_length
     validator = LengthValidator.new(validations[:length].merge(attributes: [:text]))
     validator.validate_each(self, :text, text)
+  end
+
+  def text_unique
+    unless Field.find_by_name(field_info.name).field_items.jsonb_contains(:data, text: text).empty?
+      errors.add(:text, "#{field_info.name} Must be unique")
+    end
+  end
+
+  def validate_uniqueness?
+    @validations.key? :uniqueness
   end
 
   def validate_presence?
