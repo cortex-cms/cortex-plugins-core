45835cc60a29dbb21e86df59217876f2640648ec
Alex Tharp <toastercup@gmail.com>
2016-12-21 13:26:18 -0500

    link_to_asset in asset_info_cell should open Asset URLs in a new tab
    Added ContentItemFieldType input
    
    added content item field type input
    
    added content item field type input

--- app/cells/plugins/core/content_item_cell.rb
+++ app/cells/plugins/core/content_item_cell.rb
@@ -7,8 +7,16 @@ module Plugins
 
       private
 
+      def value
+        data&.[]('content_item_id')
+      end
+
       def render_label
         "Add #{field.name}"
+      end
+
+      def render_popup
+        @options[:form].text_field 'data[content_item_id]', value: value, style: 'display:none;'
       end
     end
   end
