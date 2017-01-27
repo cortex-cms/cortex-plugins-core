d1e13e6072fb2513e54a9b43759240862c250880
ElliottAYoung <elliott.a.young@gmail.com>
2017-01-27 13:34:58 -0600

    Add #required?

--- app/cells/plugins/core/cell.rb
+++ app/cells/plugins/core/cell.rb
@@ -2,6 +2,12 @@ module Plugins
   module Core
     class Cell < FieldCell
       view_paths << "#{Cortex::Plugins::Core::Engine.root}/app/cells"
+
+      def required?
+        unless @options[:validations].blank?
+          @options[:validations][:presence] == true
+        end
+      end
     end
   end
 end
