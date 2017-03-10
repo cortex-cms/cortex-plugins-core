2726603c0dc6e803e3a755ac0703539b9217ffba
ElliottAYoung <elliott.a.young@gmail.com>
2017-03-10 15:42:41 -0600

    feat: render author cell with current_user

--- app/cells/plugins/core/author_cell.rb
+++ app/cells/plugins/core/author_cell.rb
@@ -10,6 +10,18 @@ module Plugins
       def current_user
         "#{@options[:current_user].firstname} #{@options[:current_user].lastname}"
       end
+
+      def value
+        data&.[]('author_name') || current_user
+      end
+
+      def render_label
+        @options[:form].label 'data[text]', field.name, class: 'mdl-textfield__label'
+      end
+
+      def render_input
+        @options[:form].text_field 'data[text]', value: value, placeholder: @options[:placeholder], class: 'mdl-textfield__input', required: required?
+      end
     end
   end
 end
